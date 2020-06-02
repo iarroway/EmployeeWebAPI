aa1272e2579c175a3ef1d1ba50140a81905224ee
IA <IA@DESKTOP-QE05M0J>
2020-06-02 14:38:28 -0700

    create model classes and controllers

---
+++ EmployeeWebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using EmployeeWebAPI.Models;
+
+
+namespace EmployeeWebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = " select DepartmentID, DepartmentName from dbo.Departments";
+
+            using (var con)
+        }
+    }
+}
---
+++ EmployeeWebAPI/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace EmployeeWebAPI.Models
+{
+    public class Employee
+    {
+        public long EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+        public string Mail { get; set; }
+        public DateTime? DOJ { get; set; } 
+    }
+}
